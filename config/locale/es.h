15d44bd3dc442b97de3de90ac6703d258a7ab592
Lucas Méndez <lucasmendezbaca@gmail.com>
2025-06-23 17:38:54 +0200

    ahora si

--- config/locale/es.h
+++ config/locale/es.h
@@ -1,50 +1,63 @@
-// es.h - Layout español para ZMK
-
 #pragma once
 
-#include <dt-bindings/zmk/keys.h>
+// Letras y acentos típicos del español (requiere layout Español en Windows)
+// NOTA: Estas teclas funcionan con la distribución física ANSI
 
-// Letras acentuadas (teclas muertas + vocal)
-#define ES_ACUT     LC(LS(KC_E))  // Tecla muerta de acento agudo (puede usarse sola)
-#define ES_A_ACUT   ALGR(KC_A)    // á
-#define ES_E_ACUT   ALGR(KC_E)    // é
-#define ES_I_ACUT   ALGR(KC_I)    // í
-#define ES_O_ACUT   ALGR(KC_O)    // ó
-#define ES_U_ACUT   ALGR(KC_U)    // ú
+// Ñ y acentos
+#define ES_N_TILDE     &kp SCLN        // tecla de ñ en ANSI (está donde va ;)
+#define ES_QUOT        &kp QUOTE       // comilla simple '
+#define ES_DQUO        &kp LS(QUOTE)   // comilla doble "
+#define ES_GRAVE       &kp GRAVE       // ` acento grave
+#define ES_TILDE       &kp LS(GRAVE)   // ~ virgulilla
 
-// Símbolos comunes
-#define ES_EXCL     LS(KC_1)      // !
-#define ES_QUES     LS(KC_SLASH)  // ?
-#define ES_IQU      ALGR(KC_SLASH) // ¿
-#define ES_EXM      ALGR(KC_1)     // ¡
-#define ES_PIPE     ALGR(KC_1)     // |
-#define ES_AT       ALGR(KC_2)     // @
-#define ES_HASH     ALGR(KC_3)     // #
-#define ES_DLLR     ALGR(KC_4)     // $
-#define ES_PRCNT    ALGR(KC_5)     // %
-#define ES_CIRC     ALGR(KC_6)     // ^
-#define ES_AMPS     ALGR(KC_7)     // &
-#define ES_ASTRK    ALGR(KC_8)     // *
-#define ES_LPAR     ALGR(KC_9)     // (
-#define ES_RPAR     ALGR(KC_0)     // )
+// Vocales acentuadas: debes presionar la tecla de acento ` y luego la vocal.
+// Alternativamente puedes definirlas con combinaciones si las usas mucho.
 
-// Paréntesis, llaves, corchetes
-#define ES_LBKT     ALGR(KC_LBRACKET)
-#define ES_RBKT     ALGR(KC_RBRACKET)
-#define ES_LCBR     ALGR(KC_LBRACKET) // {
-#define ES_RCBR     ALGR(KC_RBRACKET) // }
-#define ES_MINUS    KC_MINUS
-#define ES_UNDS     LS(KC_MINUS)
-#define ES_EQUAL    KC_EQUAL
-#define ES_PLUS     LS(KC_EQUAL)
-#define ES_SCLN     KC_SCLN
-#define ES_COLON    LS(KC_SCLN)
-#define ES_GRAVE    KC_GRAVE
-#define ES_TILDE    LS(KC_GRAVE)
-#define ES_BSLH     KC_BSLASH
+// Signos de puntuación
+#define ES_DOT         &kp DOT         // .
+#define ES_COMMA       &kp COMMA       // ,
+#define ES_SCLN        &kp SCLN        // ;
+#define ES_COLON       &kp LS(SCLN)    // :
 
-// Otros
-#define ES_QUOT     KC_QUOTE
-#define ES_DQUO     LS(KC_QUOTE)
-#define ES_N_TILDE  KC_SCLN       // la tecla de la ñ física en un teclado ANSI suele estar donde ';'
-#define ES_MIDDOT   ALGR(KC_3)    // ·
+// Números
+#define ES_N1          &kp N1
+#define ES_N2          &kp N2
+#define ES_N3          &kp N3
+#define ES_N4          &kp N4
+#define ES_N5          &kp N5
+#define ES_N6          &kp N6
+#define ES_N7          &kp N7
+#define ES_N8          &kp N8
+#define ES_N9          &kp N9
+#define ES_N0          &kp N0
+
+// Símbolos (requiere distribución Española en Windows)
+#define ES_EXCL        &kp LS(N1)        // !
+#define ES_IQU         &kp ALGR(SLSH)    // ¿
+#define ES_QUES        &kp LS(SLSH)      // ?
+#define ES_EXM         &kp ALGR(N1)      // ¡
+#define ES_AT          &kp ALGR(N2)      // @
+#define ES_HASH        &kp ALGR(N3)      // #
+#define ES_DLLR        &kp ALGR(N4)      // $
+#define ES_PRCNT       &kp ALGR(N5)      // %
+#define ES_CIRC        &kp ALGR(N6)      // ^
+#define ES_AMPS        &kp ALGR(N7)      // &
+#define ES_ASTRK       &kp ALGR(N8)      // *
+#define ES_LPAR        &kp ALGR(N9)      // (
+#define ES_RPAR        &kp ALGR(N0)      // )
+
+// Paréntesis, corchetes, llaves
+#define ES_MINUS       &kp MINUS         // -
+#define ES_UNDS        &kp LS(MINUS)     // _
+#define ES_EQUAL       &kp EQUAL         // =
+#define ES_PLUS        &kp LS(EQUAL)     // +
+
+#define ES_BSLH        &kp BSLH          // \
+#define ES_SLSH        &kp SLSH          // /
+#define ES_PIPE        &kp ALGR(N1)      // |
+#define ES_LBKT        &kp LBKT          // [
+#define ES_RBKT        &kp RBKT          // ]
+#define ES_LCBR        &kp LS(LBKT)      // {
+#define ES_RCBR        &kp LS(RBKT)      // }
+
+#define ES_MIDDOT      &kp ALGR(N3)      // ·
